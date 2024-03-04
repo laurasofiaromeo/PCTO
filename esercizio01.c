d825e1539dfbb7f48dea1fe011f852949ec670f2
laurasofiaromeo <162116593+laurasofiaromeo@users.noreply.github.com>
2024-03-04 16:01:44 +0000

    LR

---
+++ esercizio01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int numero ;
+    numero=12 ;
+    printf ("%d",numero) ;
+}
---
+++ esercizio03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a=7;
+    int b=13;
+    int c=a+b;
+    int d=a-b;
+    int e=a*b;
+    int f=a/b;
+   printf("%d\n",c);
+   printf ("%d\n",d);
+   printf ("%d\n",e);
+   printf ("%d\n",f);   
+}
